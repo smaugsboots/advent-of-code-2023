0117ef586f34dbe50e5a037932ca1def8d5eec79
Sameer <sameer.aggarwal2000@gmail.com>
2023-12-02 15:53:45 +0000

    Day 1

---
+++ solution-aoc23-1.1.py
@@ -0,0 +1,23 @@
+# Read input file
+input_file = open("input-aoc23-1.1.txt", "r")
+
+# Convert input to a list, with each element corresponding to a line
+input_file = input_file.readlines()
+
+# Define function to find calibration value in a line
+def calib_value(line: str) -> int:
+    digits = []
+    for char in line:
+        if char.isdigit():
+            digits.append(char)
+    value = digits[0] + digits[-1]
+    return int(value)
+
+# Find calibration value for each line
+calibration_values = []
+for line in input_file:
+    calibration_values.append(calib_value(line))
+
+# Sum calibration values
+total = sum(calibration_values)
+print(total)
---
+++ solution-aoc23-1.2.py
@@ -0,0 +1,47 @@
+from operator import itemgetter
+
+# Read input file
+input_file = open("input-aoc23-1.1.txt", "r")
+
+# Convert input to a list, with each element corresponding to a line
+input_file = input_file.readlines()
+
+def calib_value(line: str) -> int:
+    """Function to find calibration value of a line."""
+
+    digits = []
+
+    # Find numbers in digit form
+    count = 0
+    for char in line:
+        if char.isdigit():
+            digits.append([count, char])
+        count += 1
+    
+    # Find numbers in string form
+    numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for i in range(len(numbers)):
+        start = 0
+        while True:
+            index = line.find(numbers[i], start)
+            if index >= 0:
+                digits.append([index, str(i+1)])
+                start = index + 1
+            else:
+                break
+    
+    # Sort numbers in by position in the line
+    digits = sorted(digits, key=itemgetter(0))
+
+    # Calculate calibration value
+    value = digits[0][1] + digits[-1][1]
+    return int(value)
+
+# Find calibration value for each line
+calibration_values = []
+for line in input_file:
+    calibration_values.append(calib_value(line))
+
+# Sum calibration values
+total = sum(calibration_values)
+print(total)
