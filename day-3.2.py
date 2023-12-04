60ceac018bfd9bc463260932c2664dd0cd9fb3c3
Sameer <sameer.aggarwal2000@gmail.com>
2023-12-04 18:12:24 +0000

    Day 3 Part 2

--- day-3.2.py
+++ day-3.2.py
@@ -10,44 +10,46 @@ for line in input_file:
     input_matrix.append(list(line))
 input_matrix = np.array(input_matrix)
 
-def symbol_bool(char_matrix: np.array) -> np.array:
-    """Returns a boolean matrix of which cells are adjacent to a symbol."""
+def gear_bool(matrix: np.array) -> np.array:
+    """Returns two matrices:
+    1. A boolean matrix of which cells are adjacent to the first gear found
+    2. A vesrion of the input matrix with the gear that was found removed, or False if no gear was found."""
 
-    output = np.full(np.shape(char_matrix), False)
-    for i in range(len(char_matrix)):
-        for j in range(len(char_matrix[i])):
-            if char_matrix[i][j] != "." and not char_matrix[i][j].isdigit():
+    symbol_output = np.full(np.shape(matrix), False)
+    char_output = matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == "*":
+                char_output[i][j] = "."
                 for x in range(9):
                     a = (x // 3) - 1
                     b = (x % 3) - 1
                     try:
-                        output[i+a][j+b] = True
+                        symbol_output[i+a][j+b] = True
                     except:
                         pass
-    return output
+                return symbol_output, char_output
+    return symbol_output, False
 
-def digit_bool(char_matrix: np.array) -> np.array:
+def digit_bool(matrix: np.array) -> np.array:
     """Returns a boolean matrix of which cells contain digits."""
 
-    output = np.full(np.shape(char_matrix), False)
-    for i in range(len(char_matrix)):
-        for j in range(len(char_matrix[i])):
-            if char_matrix[i][j].isdigit():
+    output = np.full(np.shape(matrix), False)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j].isdigit():
                 output[i][j] = True
     return output
 
-def extract_parts(char_matrix: np.array) -> np.array:
+def extract_parts(char_matrix: np.array, truth_matrix: np.array) -> np.array:
     """Returns a matrix of digits within part numbers, i.e. numbers which are adjacent to symbols."""
 
-    symbols = symbol_bool(char_matrix)
-    digits = digit_bool(char_matrix)
-    truth_matrix = np.logical_and(symbols, digits)
     output = np.full(np.shape(char_matrix), "")
 
     # Find digits which are adjacent to symbols
     for i in range(len(truth_matrix)):
         for j in range(len(truth_matrix[i])):
-            if truth_matrix[i][j]:
+            if truth_matrix[i][j] and char_matrix[i][j].isdigit():
                 output[i][j] = char_matrix[i][j]
             else:
                 output[i][j] = " "
@@ -70,24 +72,35 @@ def extract_parts(char_matrix: np.array) -> np.array:
                         count += 1
                     else:
                         break
-
+    
     return output
 
-def sum_numbers(number_matrix: np.array) -> int:
-    """Returns the sum of numbers stored in digit form in a matrix."""
+def gear_ratio(number_matrix: np.array) -> int:
+    """Returns the gear ratio of parts adjacent to a gear."""
 
-    total = 0
+    rows = []
     for row in number_matrix:
         numbers = "".join(row).split()
-        for number in numbers:
-            total += int(number)
-    return total
+        for i in range(len(numbers)):
+            rows.append(int(numbers[i]))
+    if len(rows) == 2:
+        return np.prod(rows)
+    else:
+        return 0
 
-def sum_parts(char_matrix):
-    """Returns the sum of part numbers."""
+def sum_ratios(input_matrix: np.array) -> int:
+    """Returns the sum of gear ratios."""
 
-    parts = extract_parts(char_matrix)
-    total = sum_numbers(parts)
-    print(total)
+    sum = 0
+    digits = digit_bool
+    char_matrix = input_matrix
+    while True:
+        gear, char_matrix = gear_bool(char_matrix)
+        filter = np.logical_and(gear, digits)
+        numbers = extract_parts(input_matrix, filter)
+        sum += gear_ratio(numbers)
+        if char_matrix is False:
+            break
+    print(sum)
 
-sum_parts(input_matrix)
+sum_ratios(input_matrix)
